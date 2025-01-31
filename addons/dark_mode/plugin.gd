e838489ba775826c5ea3df1abc08e255d41af056
dmgsoftware <139420508+dmgsoftware@users.noreply.github.com>
2025-01-31 17:49:03 +0900

    Updated icon

--- addons/dark_mode/plugin.gd
+++ addons/dark_mode/plugin.gd
@@ -4,7 +4,6 @@ extends EditorPlugin
 const AUTOLOAD_NAME = "DarkMode"
 
 func _enable_plugin():
-	# The autoload can be a scene or script file.
 	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/dark_mode/dark_mode.tscn")
 
 func _disable_plugin():
